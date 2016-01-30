3811790870fab3e501c9ccee7615be882a955115
Matt Mallett <mmallet@us.ibm.com>
2016-01-30 17:25:42 -0500

    fancy manimations

---
+++ public/app.js
@@ -0,0 +1,35 @@
+(function(){
+
+  angular.module('beatsByKev', [])
+
+  .controller('LinesController', function($interval){
+
+    this.lines = [];
+
+    this.getLines = function(){
+
+    }
+
+    var that = this;
+
+    var interval = $interval(function(){
+      that.lines.push(srclines[index] + ctr++);
+      index = (index + 1) % srclines.length ;
+      if(ctr === 10){
+        $interval.cancel(interval);
+      }
+    }, 100);
+
+    var index = 0;
+    var ctr = 0;
+    var srclines = [
+      "I've been through the desert on a horse with no name.",
+      'It felt good to be out of the rain.',
+      'In the desert, you can remember your name,',
+      "for there ain't no one for to give you no pain"
+    ];
+
+
+  });
+
+})();
