eef5036bae50a008e893cfca7327ae18bb57ab4c
Matt Mallett <mmallet@us.ibm.com>
2016-01-30 18:30:17 -0500

    integration, by parts of course, with backend

--- public/app.js
+++ public/app.js
@@ -2,38 +2,66 @@
 
   angular.module('beatsByKev', [])
 
-  .controller('LinesController', function($interval){
+  .controller('LinesController', ['$interval', '$http',
+      function($interval, $http){
 
     this.lines = [];
+    this.word = null;
 
-    this.getLines = function(){
-
-    }
-
-    this.showLoading = function(){
-      return that.lines.length === 0;
-    }
+    var interval = null;
+    var loading = false;
 
     var that = this;
 
-    var interval = $interval(function(){
-      that.lines.push(srclines[index] + ctr++);
-      index = (index + 1) % srclines.length ;
-      if(ctr === 10){
-        $interval.cancel(interval);
+    function load(word, callback){
+      loading = true;
+      $http.get('song/' + word)
+      .success(function(data, status, headers, config){
+        loading = false;
+        callback(data);
+      });
+    }
+
+    this.change = function(){
+
+      if(that.word === '' || that.word === null){
+        return;
       }
-    }, 1000);
 
-    var index = 0;
-    var ctr = 0;
-    var srclines = [
-      "I've been through the desert on a horse with no name.",
-      'It felt good to be out of the rain.',
-      'In the desert, you can remember your name,',
-      "for there ain't no one for to give you no pain"
-    ];
+      if(interval){
+        $interval.cancel(interval);
+        interval = null;
+      }
+
+      that.lines = [];
+
+      load(that.word, function(data){
+
+        var index = 0;
+
+        interval = $interval(function(){
+          that.lines.push(data[index++]);
+          if(index === data.length){
+            $interval.cancel(interval);
+          }
+        }, 1000);
+      });
+    }
+
+    this.showLoading = function(){
+      return loading === true;
+    }
 
 
-  });
+
+    // var srclines = [
+    //   "I've been through the desert on a horse with no name.",
+    //   'It felt good to be out of the rain.',
+    //   'In the desert, you can remember your name,',
+    //   "for there ain't no one for to give you no pain"
+    // ];
+
+
+  }]);
 
 })();
