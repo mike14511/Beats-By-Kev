4d9ce59c8a6d08107ba2457f5b7166dc5b720544
MICHAEL D. RAMOS <ramosm@us.ibm.com>
2016-01-30 19:49:02 -0500

    Added settings.js

---
+++ settings.js
@@ -0,0 +1,10 @@
+var config = {
+  cloudant: {
+    account: process.env['cloudantDbAccount'] || 'fee7db38-e9fb-45bc-ac25-869d897a78bf-bluemix',
+    password: process.env['cloudantDbPassword'] || 'a4eb1902996d26f910bcf7b90cbe9d2741a0a9773a35ef701333f617aee3e60e',
+    apiKey:"dimandemesederyiestsedes",
+    apiPass:"190a35734edbcb2a754c2037304c33f0d082e024"
+  }
+};
+
+module.exports = config;
